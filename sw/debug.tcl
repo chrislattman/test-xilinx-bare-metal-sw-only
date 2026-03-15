2c46cbda8409778cdd88d0361bf4c44516507c4d
Chris Lattman <natgeo13@gmail.com>
2026-03-15 18:27:33 -0400

    push

--- sw/debug.tcl
+++ sw/debug.tcl
@@ -1,4 +1,5 @@
-connect # connect -host 173.673.3.2 -port 3121 if remote debugging to hw_server at 173.673.3.2:3121
+# connect -host 173.673.3.2 -port 3121 if remote debugging to hw_server at 173.673.3.2:3121
+connect
 bpremove -all
 targets 1
 rst
@@ -7,19 +8,26 @@ after 3000
 targets -set -filter {jtag_cable_name =~ "Digilent Arty Z7 003017BB13CFA" && level==0 && jtag_device_ctx=="jsn-Arty Z7-003017BB13CFA-23727093-0"}
 fpga ./hello_world/_ide/bitstream/artyz7_wrapper.bit
 targets -set -nocase -filter {name =~"APU*" && jtag_cable_name =~ "Digilent Arty Z7 003017BB13CFA" && jtag_device_ctx=="jsn-Arty Z7-003017BB13CFA-4ba00477-0"}
-loadhw -hw /home/chris/test-xilinx-bare-metal-sw-only/sw/artyz7_platform/export/artyz7_platform/hw/artyz7_wrapper.xsa -mem-ranges [list {0x40000000 0xbfffffff}]
+loadhw -hw ./artyz7_platform/export/artyz7_platform/hw/artyz7_wrapper.xsa -mem-ranges [list {0x40000000 0xbfffffff}]
 
 configparams force-mem-access 1
 targets -set -nocase -filter {name =~ "*A9*#0" && jtag_cable_name =~ "Digilent Arty Z7 003017BB13CFA" && jtag_device_ctx=="jsn-Arty Z7-003017BB13CFA-4ba00477-0"}
 rst -processor
-dow /home/chris/test-xilinx-bare-metal-sw-only/sw/artyz7_platform/export/artyz7_platform/sw/boot/fsbl.elf
+
+# Using FSBL:
+dow ./artyz7_platform/export/artyz7_platform/sw/boot/fsbl.elf
 set bp_1_46_fsbl_bp [bpadd -addr &FsblHandoffJtagExit]
 con -block -timeout 60
 bpremove $bp_1_46_fsbl_bp
 
+# Using ps7_init.tcl:
+# source ./hello_world/_ide/psinit/ps7_init.tcl
+# ps7_init
+# ps7_post_config
+
 targets -set -nocase -filter {name =~ "*A9*#0" && jtag_cable_name =~ "Digilent Arty Z7 003017BB13CFA" && jtag_device_ctx=="jsn-Arty Z7-003017BB13CFA-4ba00477-0"}
 rst -processor
-dow /home/chris/test-xilinx-bare-metal-sw-only/sw/hello_world/build/hello_world.elf
+dow ./hello_world/build/hello_world.elf
 bpadd main
 con
 configparams force-mem-access 0
--- sw/run.py
+++ sw/run.py
@@ -2,12 +2,15 @@ import time
 import xsdb
 
 session = xsdb.start_debug_session()
-session.connect() # session.connect(host="173.673.3.2", port=3121) if remote running to hw_server at 173.673.3.2:3121
-session.bpremove()
+# session.connect(host="173.673.3.2", port=3121) if remote running to hw_server at 173.673.3.2:3121
+session.connect()
+session.bpremove("--all")
 session.targets(id=1)
 session.rst()
 time.sleep(3)
 
+# To absolutely no one's surprise: AMD put out a half-baked API
+# The filter below doesn't work here despite being copied directly from debug.tcl (which works)
 session.targets("-s", filter='jtag_cable_name =~ "Digilent Arty Z7 003017BB13CFA" && level==0 && jtag_device_ctx=="jsn-Arty Z7-003017BB13CFA-23727093-0"')
 session.fpga(file="./hello_world/_ide/bitstream/artyz7_wrapper.bit")
 session.targets("-sn", filter='name =~"APU*" && jtag_cable_name =~ "Digilent Arty Z7 003017BB13CFA" && jtag_device_ctx=="jsn-Arty Z7-003017BB13CFA-4ba00477-0"')
